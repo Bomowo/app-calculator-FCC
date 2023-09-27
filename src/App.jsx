2aa6bffc7ba4d8f8cdccd090e95f7acbd217870f
Bomowo <oeccc25@yahoo.com>
2023-09-27 18:15:54 +0300

    separate calculator reset and add calculation

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,30 @@ function App() {
     formula: ''
   })
 
-  function testClick (e) {
-    if (e.target.value === 'AC') {
-      setCalculator({
-        display: '',
-        formula: ''
-      })
-    }
+  function resetCalculator () {
+    setCalculator({
+      display: '',
+      formula: ''
+    })
+  }
 
+  function handleNumbers (e) {
     setCalculator(prevCalculator => {
       return {
         ...prevCalculator,
         display: e.target.value,
         formula: prevCalculator.formula + e.target.value
+      }
+    })
+  }
+
+  function calculate() {
+    const answer = eval(calculator.formula)
+    setCalculator(prevCalculator => {
+      return {
+        ...prevCalculator,
+        display: answer,
+        formula: prevCalculator.formula + '=' + answer
       }
     })
   }
@@ -30,23 +41,23 @@ function App() {
         <div>input and output Display: {calculator.display}</div>
       </div>
       <div className="buttons-container">
-        <button value="AC" onClick={testClick}>AC</button>
-        <button value="/" onClick={testClick}>/</button>
-        <button value="X" onClick={testClick}>X</button>
-        <button value="-" onClick={testClick}>-</button>
-        <button value="7" onClick={testClick}>7</button>
-        <button value="8" onClick={testClick}>8</button>
-        <button value="9" onClick={testClick}>9</button>
-        <button value="+" onClick={testClick}>+</button>
-        <button value="4" onClick={testClick}>4</button>
-        <button value="5" onClick={testClick}>5</button>
-        <button value="6" onClick={testClick}>6</button>
-        <button value="1" onClick={testClick}>1</button>
-        <button value="2" onClick={testClick}>2</button>
-        <button value="3" onClick={testClick}>3</button>
-        <button value="=" onClick={testClick}>=</button>
-        <button value="0" onClick={testClick}>0</button>
-        <button value="." onClick={testClick}>.</button>
+        <button value="AC" onClick={resetCalculator}>AC</button>
+        <button value="/" onClick={handleNumbers}>/</button>
+        <button value="X" onClick={handleNumbers}>X</button>
+        <button value="-" onClick={handleNumbers}>-</button>
+        <button value="7" onClick={handleNumbers}>7</button>
+        <button value="8" onClick={handleNumbers}>8</button>
+        <button value="9" onClick={handleNumbers}>9</button>
+        <button value="+" onClick={handleNumbers}>+</button>
+        <button value="4" onClick={handleNumbers}>4</button>
+        <button value="5" onClick={handleNumbers}>5</button>
+        <button value="6" onClick={handleNumbers}>6</button>
+        <button value="1" onClick={handleNumbers}>1</button>
+        <button value="2" onClick={handleNumbers}>2</button>
+        <button value="3" onClick={handleNumbers}>3</button>
+        <button value="=" onClick={calculate}>=</button>
+        <button value="0" onClick={handleNumbers}>0</button>
+        <button value="." onClick={handleNumbers}>.</button>
       </div>
     </div>
   )
